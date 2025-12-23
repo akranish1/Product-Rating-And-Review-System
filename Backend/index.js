e3a06a27bb7db94e65df9acdbf538e87cb50ba03
akranish1 <akranish0@gmail.com>
2025-12-23 10:28:20 +0530

    d2x

--- Backend/index.js
+++ Backend/index.js
@@ -122,7 +122,7 @@ app.post("/auth/signup", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,        // true in production (HTTPS)
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
