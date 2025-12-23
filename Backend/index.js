db94aea78cdf9e619d77f07b5bc12d49dcf8d875
akranish1 <akranish0@gmail.com>
2025-12-23 10:57:10 +0530

    d3x

--- Backend/index.js
+++ Backend/index.js
@@ -17,9 +17,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cookieParser());
+
 app.use(express.static("dist"));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
