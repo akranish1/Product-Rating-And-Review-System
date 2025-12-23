baa30846dac65bc2b80c7f64eb39aec412a2402f
akranish1 <akranish0@gmail.com>
2025-12-23 09:43:52 +0530

    d5

--- frontend/App/src/App.jsx
+++ frontend/App/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
         {/* Route WITHOUT navbar + footer */}
         <Route path="/auth" element={<Login />} />
-        <Route path="/auth/SignUp" element={<SignUp/>} />
+        <Route path="/auth/signup" element={<SignUp/>} />
 
       </Routes>
     </BrowserRouter>
